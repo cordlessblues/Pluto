c2becdb42c25829a6fb7ab594ffe9efe2214564d
Carl <carlankorhillam@gmail.com>
2024-08-23 21:51:08 -0500

    updated install.sh to cp the desktop file to ~/.local/share/applications/com.cordlessblues.Pluto.desktop

--- Install.sh
+++ Install.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.config/Pluto/
 mv ~/Documents/Pluto/Config.Json ~/.config/Config.Json
 mv ~/Documents/Pluto/Pluto.png ~/.config/Pluto.png
 mv ~/Documents/Pluto/Pluto.py /opt/Pluto/
+cp ~/Documents/Pluto/com.cordlessblues.Pluto.desktop ~/.local/share/applications/com.cordlessblues.Pluto.desktop
 echo "install complete"
 clear
 python3 ~/Documents/Pluto/Pluto.py
